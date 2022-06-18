201a57c9cf0063473729212b4fffce7ac1500c51
Anirudh Acharya <anirudhacharya2001@gmail.com>
2022-06-18 17:42:50 +0530

    Textform modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/textform';
 
@@ -6,10 +7,10 @@ function App() {
   return (
     <>
     <Navbar title="TextUtils" about="About" home="Home" />
-    <div className="container my-3">
+    <div className="container my-2">
     <TextForm heading="Enter the text to analyse"/>
     </div>
-   
+    <About className="container my-2"/>
     </>
   );
 }
--- src/components/textform.js
+++ src/components/textform.js
@@ -10,9 +10,37 @@ export default function TextForm(props) {
     const handleOnChange = (event)=>{
         setText(event.target.value)
     }
-    const handlelowclick = ()=>{
+    const handleLowClick = ()=>{
       let rtext=text.toLowerCase()
       setText(rtext)
+    }
+    const handleClearClick = ()=>{
+      setText("")
+    }
+    const handleTitleClick = ()=>{
+      let str=text.split(" ")
+      for (let i = 0; i < str.length; i++){
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1).toLowerCase();
+      }
+      setText(str.join(" "))
+      
+    }
+    const handleInverseClick = ()=>{
+      let str=text.split(" ")
+      for (let i = 0; i < str.length; i++){  
+        let ptr=str[i].split("");
+          for (let j=0; j<ptr.length;j++){
+            if(j%2===0){ 
+             ptr[j] = ptr[j].charAt(0).toUpperCase();
+            }
+            if(j%2!==0){
+              ptr[j] = ptr[j].charAt(0).toLowerCase();
+            } 
+          }
+        str[i]=ptr.join("");
+      }
+      setText(str.join(" "))
+      
     }
   return (
     <>
@@ -22,8 +50,11 @@ export default function TextForm(props) {
             <textarea className="form-control" value={text} placeholder="Enter Text Here" id="Textarea" rows="8" onChange={handleOnChange}></textarea>
             </div>
             <div className='d-flex container  justify-content-between'>
-              <button className="btn btn-info" onClick={handleUpClick}>Convert To Uppercase</button>
-              <button className="btn btn-info" onClick={handlelowclick}> Convert To Lowercase</button>
+              <button className="btn btn-info" onClick={handleUpClick}>Convert To UPPERCASE</button>
+              <button className="btn btn-info" onClick={handleLowClick}> Convert To lowercase</button>
+              <button className="btn btn-info" onClick={handleTitleClick}>Convert to TitleCase</button>
+              <button className="btn btn-info" onClick={handleInverseClick}>Convert to InVeRsEcAsE</button>
+              <button className="btn btn-info" onClick={handleClearClick}>Clear Text</button> 
             </div>  
     </div>
     <div className="container my-5">
