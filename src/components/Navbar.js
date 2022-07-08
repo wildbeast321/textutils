b9585a8bf471b8d0bea84fd8092379ea325df363
Anirudh Acharya <anirudhacharya2001@gmail.com>
2022-07-08 13:08:22 +0530

    Link changed to NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 
 export default function Navbar(props) {
 
-
+ let activeClassName="active nav-link"
   
   return (
 <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">{props.title}</Link>
+    <NavLink className="navbar-brand" to="/">{props.title}</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/">{props.home}</Link>
+          <NavLink className={({ isActive }) =>
+              isActive ? activeClassName : "nav-link"} aria-current="page" to="/">{props.home}</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/About">{props.about}</Link>
+          <NavLink className={({ isActive }) =>
+              isActive ? activeClassName : "nav-link"}  to="/About">{props.about}</NavLink>
         </li>
       </ul>
     <div className="form-check form-switch ">
